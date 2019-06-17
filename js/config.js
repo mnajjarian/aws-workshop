050a0b003002a0ee3b9c1c29bc8c4f9cecee84ed
mnajjarian <mnajarian@gmail.com>
2019-06-17 08:30:37 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_1aj5mT34p', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'h6414t64h0htu0lgkailu9vml', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
